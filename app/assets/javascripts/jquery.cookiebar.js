5f639428d4cbd23b26f0b3a7a8366d1586d153f0
brandenmittra <branden@univrs.co.uk>
2015-12-22 10:33:01 +0000

    test error 500 update

--- app/assets/javascripts/jquery.cookiebar.js
+++ app/assets/javascripts/jquery.cookiebar.js
@@ -38,7 +38,7 @@
 			acceptOnScroll: false, //Set to true to accept cookies when visitor scrolls X pixels up or down
 			acceptAnyClick: false, //Set to true to accept cookies when visitor clicks anywhere on the page
 			expireDays: 365, //Number of days for cookieBar cookie to be stored for
-			renewOnVisit: false, //Renew the cookie upon revisit to website
+			renewOnVisit: true, //Renew the cookie upon revisit to website
 			forceShow: false, //Force cookieBar to show regardless of user cookie preference
 			effect: 'slide', //Options: slide, fade, hide
 			element: 'body', //Element to append/prepend cookieBar to. Remember "." for class or "#" for id.
